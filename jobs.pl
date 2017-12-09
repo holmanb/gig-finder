f97a566ad0d9b89888c91310e4fa7fb46c501926
Brett Holman <bpholman5@gmail.com>
2017-12-09 05:40:51 +0000

    Starting to create the yaml config file

--- jobs.pl
+++ jobs.pl
@@ -2,58 +2,73 @@
 use strict;
 use warnings;
 use local::lib;
-use LWP::UserAgent ();
 use feature 'say';
+use LWP::Simple;
+my $site = "https://santafe.craigslist.org/search/acc";
 
-# Function for getting html for any arbitrary website 
-sub getHtmlPage{
-
-	# Takes a URL and (optionally) an agent
-	my @args = @_;
-
-	# Initialize vars 	
-	my $num_args = scalar(@args);   # number of args
-	my $ua = "";		  	# user agent
-	my $url = $args[0];		# url
-	say "Searching for: $url";	# user notification
-
-	# Function can only handle two args
-	if($num_args>2){ 
-		die "Incorrectly used function";
+# Reduce all URLs to standard format
+sub standardize_urls {
+	my $source = $_[0];
+	my $canon = "";
+	return ($canon) if ($source =~ /^(mailto:|telnet:|callto:)/);
+	if($source=~ /^\//) {
+		$source = $site . $source;
 	}
-
-	# UserAgent isn't given, so make one
-	elsif($num_args==1){
-
-		# hash of ssl options for https
-		my %ssl_ops = (
-			verify_hostname => 1 ,
-			protocols_allowed => ['https']
-			);
-
-		# make the user agent
-		$ua = LWP::UserAgent->new(ssl_opts => \%ssl_ops);
-		$ua->timeout(10);
-		$ua->env_proxy;
-	}
-
-	# UserAgent is given as argument
-	else{
-		$ua = $args[1];
-	}
-
-	# HTTP GET REQUEST
-	my $response = $ua->get($url);
-
-	# Unsuccessful request
-	if (!$response->is_success){
-		say $response->as_string;
-		die "Unsuccessful request $response->status_line";
-	}
-
-	# Successful request
-	return $response->decoded_content;
+	$source =~ s/[\#?].*//;
+	return $source;
 }
+
+# Craigslist filter
+sub filterCraigslist{
+	my @urls = @_;
+	my @goodness;
+	my $re = "\.html";
+	for my $url(@urls){
+		if($url =~ /$re/){
+			push(@goodness, $url);
+		}
+	}
+	return @goodness;
+}
+
+# Get outward facing links that may appear interesting
+sub getLinks{
+	my $html;
+	my @inputs=@_;
+	my @urls;
+
+	# Grab a web page and check the links 
+	foreach my $page(@inputs){
+		print "Checking $page\n";
+		$html = get($page);
+		@urls = $html =~ /\shref="?([^\s>"]+)/gi;
+	}
+
+	my %counter;
+	my @externals = ();
+	my $full;
+
+	# Remove sites that start with the same url as the input
+	foreach my $url(@urls){
+		$full = standardize_urls($url);
+		next unless($full);
+		
+		if($full !~ /^$site/) {
+			push @externals, $full;
+			$counter{$full}++;
+		}
+	}
+
+	# Print and return urls
+	my @interesting_urls;
+	print "Websites found on @inputs\n";
+	foreach my $url(keys %counter){
+		#print "$url\n";
+		push(@interesting_urls, $url);
+	}
+	return @interesting_urls;
+}
+
 
 # Uses ssmtp to send an email
 sub sendEmail{
@@ -74,10 +89,24 @@ sub sendEmail{
 }
 
 # Main execution
-my $url =  "https://santafe.craigslist.org/search/acc";
-my $response = getHtmlPage($url); 
-say $response;
-sendEmail('holmanb0214@my.uwstout.edu', 'Found a job!', $response);
+my $search_url =  "https://santafe.craigslist.org/search/acc";
+#my $response = getHtmlPage($url); 
+#say $response;
+
+
+# Prevent duplicate sends by storing sent jobs in file
+my $filename = ".sent_jobs";
+my $handle = undef;
+open($handle, "<", $filename) or say "No jobs have been stored in $filename yet. Will generate this file if jobs are found.\n";
+
+# Search given website for interesting urls
+my @potential_opportunities = filterCraigslist(getLinks(($search_url)));
+my $url = $potential_opportunities[0];
+
+say "html from the first site";
+say get($url);
+sendEmail('holmanb0214@my.uwstout.edu', "Found a job!", $url);
+
 
 # Inside of the ul class="rows", I need to pick up all of the href links in <li class="result-row">
 # These links should be stored in an array and then subsequently checked to see if they match the requirements
