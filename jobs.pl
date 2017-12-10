820914724c0b9984e05c17dfe89e169d9cbced79
Brett Holman <bpholman5@gmail.com>
2017-12-10 09:34:27 +0000

    Added cache writing, need to provide cli options to clear and display cache

--- jobs.pl
+++ jobs.pl
@@ -2,14 +2,41 @@
 use strict;
 use warnings;
 use feature 'say';
-use YAML::Tiny;
 use local::lib;
+use YAML::Tiny;
 use LWP::Simple;
-my $site = "https://santafe.craigslist.org/search/acc";
+use Getopt::Long qw(GetOptions);
 
-# Reduce all URLs to standard format
+# Getting and hanlding cli options
+my $clear_cache;
+my $dry_run;
+my $past;
+my $help;
+GetOptions(
+       "clear_cache"|"c" => \$clear_cache,
+       "past"|"p" => \$past,
+       "dry_run"|"d" => \$dry_run,
+       "help"|"h" => \$help,
+) or die "Usage: $0 --one_time --clear_cache";
+
+# Dry run used for dependency checking on the install script
+if($dry_run){
+        say "Dry run, dependencies properly installed.";
+}
+
+&main;
+
+# standardize_urls
+# Reduces all URLs to a standard format
+#
+# Takes:
+# - $url - a string that should hold a url
+# Returns:
+# - $source - a formatted url
+##
 sub standardize_urls {
 	my $source = $_[0];
+        my $site = $_[1];
 	my $declaration = "";
 	return ($declaration) if ($source =~ /^(mailto:|telnet:|callto:)/);
 	if($source=~ /^\//) {
@@ -19,11 +46,20 @@ sub standardize_urls {
 	return $source;
 }
 
-# Craigslist filter
-sub filterCraigslist{
+# filter_craigslist
+# removes all URLS that are not *.html
+#
+# Takes:
+# - @urls - an array with scalars of urls
+# Returns:
+# - @goodness - sweet, sweet, chocolatey goodness
+##
+sub filter_craigslist{
 	my @urls = @_;
 	my @goodness;
 	my $re = "\.html";
+
+        # Check for *.html
 	for my $url(@urls){
 		if($url =~ /$re/){
 			push(@goodness, $url);
@@ -32,10 +68,17 @@ sub filterCraigslist{
 	return @goodness;
 }
 
-# Get outward facing links that may appear interesting
-sub getLinks{
+# get_links
+# extract links from the urls in the array passed to the site, also filters self-referencing pages
+#
+# Takes:
+# - @inputs - a list of urls
+# Returns:
+# - @interesting_urls - an array of standardized, 
+sub get_links{
 	my $html;
-	my @inputs=@_;
+	my @inputs=$_[0];
+        my $site = $_[1];
 	my @urls;
 
 	# Grab a web page and check the links 
@@ -50,7 +93,7 @@ sub getLinks{
 
 	# Remove sites that start with the same url as the input
 	foreach my $url(@urls){
-		$full = standardize_urls($url);
+		$full = standardize_urls($url,$site);
 		next unless($full);
 		
                 # Save sites that 
@@ -62,31 +105,43 @@ sub getLinks{
 
 	# Print and return urls
 	my @interesting_urls;
-	#print "Websites found on @inputs\n";
 	foreach my $url(keys %counter){
-	#	print "$url\n";
 		push(@interesting_urls, $url);
 	}
 	return @interesting_urls;
 }
 
 
+
+# get_links
 # Uses ssmtp to send an email
+#
+# Takes:
+# - $destination - email address
+# - $subject - email subject
+# - $body - email body
 sub sendEmail{
 
 	my ($destination, $subject, $body) = @_;
 
         # Create and display the shell command used to send the email
 	my $command="echo \'$body\' | mail -s \'$subject\' $destination";
-	#say "\nExecuting shell command: $command";
 	my $output = qx($command);
 }
 
+# keyword_search
 # does a keyword search based on a hash of keywords passed in: returns true if it passes the test 
+# 
+# Takes:
+# - $found_url - web page to be scraped
+# - $find - desirable keywords, one of these keywords must be found, or () is returned
+# - $exclude - undesirable keywords, if one of these keywords is found, () is returned
+# Returns:
+# - @keywords - a list of the desirable keywords that were found in this webpage
+##
 sub keyword_search{
 	my ($found_url, $find, $exclude) = @_;
-        my $found = 0;
-        my $skip = 0; 
+        my @keywords=();
 
         # getting the final webpage
 	my $html = get($found_url);
@@ -95,9 +150,9 @@ sub keyword_search{
         foreach my $find_item (@$find){
 
                 if($html =~ /$find_item/){
-                
-                        $found=1;
-                        next;
+                        
+                        # Save keywords for later
+                        push(@keywords, $find_item);
                 }
         }
 
@@ -105,67 +160,79 @@ sub keyword_search{
         foreach my $exclude_item (@$exclude){
 
                 if($html =~ /$exclude_item/) {
-                        $skip = 1;
-                        return 0;
+                        return ();
                 }
         }
-        return $found;
+        return @keywords;
 }
 
-# Open the config
-my $yaml = YAML::Tiny->read('.config.yml');	
 
-# Get a reference to the first document
-my $config = $yaml->[0];
-
-my $search_url =  "https://santafe.craigslist.org/search/acc";
-
-# Loop through config  
-while (my($person_key, $person_value) = each %$config){
- 	say "Searching for jobs for $person_value->{name}";	
-	
-	my @found_jobs;
-
-	# Search each profile
-	my $profiles = $person_value->{profiles};
-	while (my($key, $profile_value) = each %$profiles){
-		
-                # Get each location url in the config file
-		my @locations = $profile_value->{location_urls};
-		for(my $j=0;$j<scalar(@locations);$j++){
-			my $config_urls = $profile_value->{location_urls}[$j];
-			say "Searching $config_urls";
-
-			# Search given website for interesting urls
-			my @potential_opportunities = filterCraigslist(getLinks($config_urls));
-                        foreach my $opportunity (@potential_opportunities){
-                                
-                                # Match found! 
-                                if(keyword_search($opportunity, ($profile_value->{keywords}->{find}), $profile_value->{keywords}->{exclude})){
-                                       push @found_jobs, $opportunity; 
-                                       say "Found a job!! $opportunity"
-                                }
-                        }
-		}		
- 	}	
+sub main{
         
-        # Found some jobs
-        if(scalar(@found_jobs)){
-               
-                # Send emails and stuff
-                my @emails=$person_value->{email};
-                my @tags = ("Your Strangely Human Perl Script", "Your Anonymous Benefactor", "Your Null Friend", "Your Most Favoured Servant");
-                my $msg="Hey $person_value->{name}!\n\nCheck out these sweet jobs I found for you!\n\n";
-                $msg.=join("\n", @found_jobs)."\n\nSincerely,\n\n".$tags[int(rand(scalar(@tags)-1))];
+        # Open the config
+        my $yaml = YAML::Tiny->read('.config.yml');	
 
-                for(my $i=0;$i<scalar(@emails);$i++){
+        # Get a reference to the first document
+        my $config = $yaml->[0];
+
+        # Loop through config  
+        while (my($person_key, $person_value) = each %$config){
+                say "Searching for jobs for $person_value->{name}";	
+                
+                my @found_jobs;
+
+                # Search each profile
+                my $profiles = $person_value->{profiles};
+                while (my($key, $profile_value) = each %$profiles){
                         
-                        # Sending emails
-                        say "Sending emails to $person_value->{email}[$i]";
-                        sendEmail($person_value->{email}[$i], "Found a job!",$msg);
+                        # Get each location url in the config file
+                        my @locations = $profile_value->{location_urls};
+                        for(my $j=0;$j<scalar(@locations);$j++){
+                                my $config_urls = $profile_value->{location_urls}[$j];
+                                say "Searching $config_urls";
+
+                                # Search given website for interesting urls
+                                my @potential_opportunities = filter_craigslist(get_links($config_urls, $config_urls));
+                                foreach my $opportunity (@potential_opportunities){
+                                        
+                                        # Match found! 
+                                        my @keywords = keyword_search($opportunity, ($profile_value->{keywords}->{find}), $profile_value->{keywords}->{exclude});
+                                        if(@keywords){
+                                               push @found_jobs, $opportunity; 
+                                               say "Found a job!! keywords[". join(", ", @keywords) ."] $opportunity"
+                                        }
+                                }
+                        }		
+                }	
+                
+                # Found some jobs
+                if(scalar(@found_jobs)){
+
+                        # Compare to old jobs 
+                        open(DATA, "<.cache") or say "No cache found";
+                        while(<DATA>){
+                               for my $i (0..scalar(@found_jobs)-1){
+                                        if($_ eq $found_jobs[$i]){
+                                                splice(@found_jobs, $i, 1);
+                                                say "match found, removing $found_jobs[$i]";
+                                        }
+                               }
+                        }
+                        # Send emails and stuff
+                        my @emails=$person_value->{email};
+                        my @tags = ("Your Strangely Human Perl Script", "Your Anonymous Benefactor", "Your Null Friend", "Your Most Favoured Servant");
+                        my $msg="Hey $person_value->{name}!\n\nCheck out these sweet jobs I found for you!\n\n";
+                        $msg.=join("\n", @found_jobs)."\n\nSincerely,\n\n".$tags[int(rand(scalar(@tags)-1))];
+
+                        for(my $i=0;$i<scalar(@emails);$i++){
+                                
+                                # Sending emails
+                                say "Sending emails to $person_value->{email}[$i]";
+                                sendEmail($person_value->{email}[$i], "Found a job!",$msg);
+                        }
+                }else{
+                        say "\nNo jobs this time :(";
                 }
-        }else{
-                say "\nNo jobs this time :(";
         }
 }
 
